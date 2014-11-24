4b264612cf0c3b3efc0846d9b06e788dcbd81b89
carolinedanzi <danzicr@miamioh.edu>
2014-11-24 13:53:17 -0500

    Worked on increaseCount

--- gagecaroline.cpp
+++ gagecaroline.cpp
@@ -3,12 +3,17 @@
 void gagecaroline::increaseCount(std::string s, int amount){
 	// If the word has already been seen, update the count
 	if (stringOrg.keyExists(s)){
-		// The new count will be the old plus the new amount
-		int newCount = getCount(s) + amount;
-		stringOrg.changeValue(s, newCount);
+		// Find the index in the popularity vector and
+		// update the count that is stored there
+		int indexInArray = stringOrg.find(s);
+		countOrg[indexInArray].second += amount;
+		bubbleUp(indexInArray);
 	}
 	else{
-
+		countOrg.push_back(std::make_pair(s, amount));
+		int index = countOrg.size() - 1;
+		stringOrg.add(s, index);
+		bubbleUp(index);
 	}
 }
 
@@ -32,13 +37,20 @@ unsigned int gagecaroline::numEntries(){
 void gagecaroline::bubbleUp(unsigned int index){
 	std::pair<std::string, int> temp;
 	
-	if(countOrg[index] > countOrg[index-1]){
+	// If the count has exceeded the count of the previous
+	// element, we need to swap them so the more popular
+	// element is first in the vector
+	if(countOrg[index].second > countOrg[index-1].second){
 		temp = countOrg[index];
 		countOrg[index] = countOrg[index-1];
 		countOrg[index-1] = temp;
-		
+		// After swapping the pairs in the popularity vector,
+		// we need to make sure the hash table contains
+		// the new index in the popularity vector
+		stringOrg.changeValue(countOrg[index].first, index - 1);
 		bubbleUp(index-1);
 	}
+
 
 
 
--- gagecaroline.h
+++ gagecaroline.h
@@ -7,10 +7,10 @@ public:
 	void increaseCount(std::string s, int amount);
 	unsigned int getCount(std::string s);
 	std::string getNthPopular(unsigned int n);
-	unsigned int numEntries();
-	void bubbleUp(unsigned int index);
+	unsigned int numEntries();	
 
 private: 
 	std::vector<std::pair<std::string, int>> countOrg;
 	HashTable<std::string, int> stringOrg;
+	void bubbleUp(unsigned int index);
 };
