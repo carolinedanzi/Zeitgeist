2f5855638f20fd1d8fcbcadc217f84f3a96b87de
carolinedanzi <danzicr@miamioh.edu>
2014-11-23 21:23:27 -0500

    Added some comments

--- gagecaroline.cpp
+++ gagecaroline.cpp
@@ -8,10 +8,14 @@ unsigned int gagecaroline::getCount(std::string s){
 	return 0;
 }
 
+// The vector is organized by popularity, where the nth 
+// most popular is located at index n. We just need to 
+// return the first object in the pair, which is the word.
 std::string gagecaroline::getNthPopular(unsigned int n){
 	return countOrg[n].first;
 }
 
+// Returns the size of the hash table where all the words are stored
 unsigned int gagecaroline::numEntries(){
 	return stringOrg.size();
 }
